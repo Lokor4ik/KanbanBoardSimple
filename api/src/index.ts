561f4396eef3225a23b03873a4da2392eba6cba5
Lokor4ik <lokorinfantem@gmail.com>
2021-04-22 19:17:25 +0300

    updated structure for heroku

--- api/src/index.ts
+++ api/src/index.ts
@@ -28,7 +28,7 @@ connectDB();
 
 if (require.main === module) {
   try {
-    app.listen(API_PORT, () => {
+    app.listen(Number(API_PORT), '0.0.0.0', () => {
       console.warn(`API server listening on port ${API_PORT}`);
     });
   } catch (error) {
